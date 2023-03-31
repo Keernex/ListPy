57be0a938e8ed1ffdb06618bf0012cd14b53e1f3
Кушніренко Максим <100068913+Keernex@users.noreply.github.com>
2023-03-31 17:35:43 +0000

    done Ex-3

---
+++ main.py
@@ -0,0 +1,8 @@
+import random
+
+listrandom = ["JavaScript", "рядки", "призначені", "для", "зберігання", "та", "обробки", "тексту."]
+print(listrandom)
+q=[]
+for i in listrandom:
+  q.append(int(len(i)))
+print(q)
