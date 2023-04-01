a72260d078a74034904f7c72c715e38fad0ccc56
Кушніренко Максим <100068913+Keernex@users.noreply.github.com>
2023-04-01 14:33:19 +0000

    done Ex-4

--- main.py
+++ main.py
@@ -9,9 +9,10 @@ listrandom2 = []
 for i in range(11):
   listrandom2.append(random.randint(-10, 10))
 print("list random 2 - ", listrandom2)
-list=listrandom+listrandom2
+
+list = listrandom + listrandom2
 l = []
 for i in list:
-    if i not in l:
-        l.append(i)
+  if i not in l:
+    l.append(i)
 print(l)
