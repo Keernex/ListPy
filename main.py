11f50390bc03bb76f4c17dea1534feded541425e
Кушніренко Максим <100068913+Keernex@users.noreply.github.com>
2023-04-01 14:28:44 +0000

    done Ex-2

--- main.py
+++ main.py
@@ -5,8 +5,8 @@ for i in range(11):
   listrandom.append(random.randint(-10, 10))
 print("list random - ", listrandom)
 
-for i in range(0, len(listrandom)):
-  if listrandom.count(listrandom[i])>1:
-    for j in listrandom:
-      if listrandom[i] == j and listrandom.count(listrandom[i])>1:
-        listrandom.pop(i)
+l = []
+for i in listrandom:
+    if i not in l:
+        l.append(i)
+print(l)
